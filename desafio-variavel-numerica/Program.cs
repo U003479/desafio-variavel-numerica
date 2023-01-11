4a7a8f3b60a31835a9ab823b5e20ceb7062d9385
Carmen Lopez <carmen.lopez@neon.com.br>
2023-01-11 16:54:16 -0300

    desafio carmen

--- desafio-variavel-numerica/Program.cs
+++ desafio-variavel-numerica/Program.cs
@@ -5,11 +5,11 @@ Console.WriteLine("\n -- Desafio 1 -- \n");
 
 int num1 = 21474831;
 
-int num2 = 2147483648;
+uint num2 = 2147483648;
 
-int num3 = num1 + num2;
+float num3 = num1 + num2;
 
-int resultado = num3 / 5000002;
+float resultado = num3 / 5000002;
 
 Console.WriteLine("Numero 1: " + num1);
 Console.WriteLine("Numero 2: " + num2);
@@ -18,32 +18,32 @@ Console.WriteLine(num3 + " / " + 5000002 + " = " + resultado);
 
 //--------------------------------------------------------
 Console.WriteLine("\n -- Desafio 2 -- \n");
-// RESOLVA TODOS OS ERROS DE COMPILAÇÃO ABAIXO.
-// O PROGRAMA DEVE EXIBIR O RESULTADO 433,79153 NA TELA
+//// RESOLVA TODOS OS ERROS DE COMPILAÇÃO ABAIXO.
+//// O PROGRAMA DEVE EXIBIR O RESULTADO 2147483643 NA TELA
 
 uint val1 = 10;
 
 short val2 = -15;
 
-uint val3 = val1 + val2;
+long val3 = val1 + val2;
 
-int resultado1 = (float)num3 + 2147483648;
+long resultado1 = (int)val3 + 2147483648;
 
-Console.WriteLine("Numero 1: " + num1);
-Console.WriteLine("Numero 2: " + num2);
-Console.WriteLine(num1 + " + " + num2 + " = " + num3);
-Console.WriteLine(num3 + " / " + 5000002 + " = " + resultado);
+Console.WriteLine("Numero 1: " + val1);
+Console.WriteLine("Numero 2: " + val2);
+Console.WriteLine(val1 + " + (" + val2 + ") = " + val3);
+Console.WriteLine(val3 + " + " + 2147483648 + " = " + resultado1);
 
 
-//--------------------------------------------------------
+////--------------------------------------------------------
 Console.WriteLine("\n -- Desafio 3 -- \n");
-// LUCAS RECEBE 50000 E QUER SABER QUANTO IRÁ GANHAR DE FGTS POR MÊS
-// DESENVOLVA UM PROGRAMA QUE CALCULE O FGTS DO LUCAS 
-// Formula: salario * 8% = valorFgts;
+//// LUCAS RECEBE 50000 E QUER SABER QUANTO IRÁ GANHAR DE FGTS POR MÊS
+//// DESENVOLVA UM PROGRAMA QUE CALCULE O FGTS DO LUCAS 
+//// Formula: salario * 8% = valorFgts; 4000,04
 
 float salario = 50000.50f;
 
-var fgts = 0;
+var fgts = (decimal)salario * 8/100;
 
 Console.WriteLine("Salario: " + salario);
 Console.WriteLine("FGTS: " + fgts);
